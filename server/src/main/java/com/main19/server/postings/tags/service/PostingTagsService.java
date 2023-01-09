b812c66fe28ab4f620ba18ac57c2a44ac0b54187
gimhae-person <hyeino3o@gmail.com>
2023-01-09 21:03:24 +0900

    Feat : PostingTags 수정 기능 추가

--- server/src/main/java/com/main19/server/postings/tags/service/PostingTagsService.java
+++ server/src/main/java/com/main19/server/postings/tags/service/PostingTagsService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.main19.server.postings.entity.Posting;
+import com.main19.server.postings.repository.PostingRepository;
 import com.main19.server.postings.service.PostingService;
 import com.main19.server.postings.tags.entity.PostingTags;
 import com.main19.server.postings.tags.entity.Tag;
 import com.main19.server.postings.tags.repository.PostingTagsRepository;
+import com.main19.server.utils.CustomBeanUtils;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,11 +27,27 @@ public class PostingTagsService {
 		Tag tag	 = tagService.findTag(tagName);
 		postingTags.setTag(tag);
 
-		// for (int i = 0; i < tagName.size(); i++){
-		// 	Tag tag	 = tagService.findTag(tagName.get(i));
-		// 	postingTags.setTag(tag);
-		// }
+		return postingTagsRepository.save(postingTags);
+	}
+
+	public PostingTags updatePostingTags(PostingTags postingTags, long postingId, String tagName) {
+		List<PostingTags> getpostingTags = findPostingTagsByPostingId(postingId);
+		// 수정 시 기존에 있던거 싹 삭제하고 가져오도록 for문 돌렸는데 더 좋은 방법이 있을까요..
+		for(int i = 0; i < getpostingTags.size(); i++) {
+			postingTagsRepository.delete(getpostingTags.get(i));
+		}
+
+		Posting posting = postingService.findVerifiedPosting(postingId);
+		postingTags.setPosting(posting);
+
+		Tag tag	 = tagService.findTag(tagName);
+		postingTags.setTag(tag);
 
 		return postingTagsRepository.save(postingTags);
+	}
+
+	private List<PostingTags> findPostingTagsByPostingId(long postingId) {
+		List<PostingTags> getpostingTags = postingTagsRepository.findAllByPosting_PostingId(postingId);
+		return getpostingTags;
 	}
 }
