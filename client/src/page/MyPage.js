43fad5633b8d3fb7ea01ae60a4d88d810e7a23ab
kanglaeheon <kanglaeheon@gmail.com>
2023-01-09 18:02:05 +0900

    Feat: MyPage/MyPlants 컴포넌트 초기 구현

--- client/src/page/MyPage.js
+++ client/src/page/MyPage.js
@@ -1,10 +1,20 @@
+import styled from "styled-components";
+
+import MyPlants from "../components/MyPage/MyPlants";
 import UserInfo from "../components/MyPage/UserInfo";
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 750px;
+`;
 
 const MyPage = () => {
   return (
-    <>
+    <StyledContainer>
       <UserInfo />
-    </>
+      <MyPlants />
+    </StyledContainer>
   );
 };
 
