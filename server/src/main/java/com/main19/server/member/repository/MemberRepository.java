ce13ceb36685c1ca2721f2912e41d9ca7bd74413
TaeHeyeon_Kim <thk98k@gmail.com>
2023-01-09 12:11:38 +0900

    Feat : MemberRepository findByEmail 메서드 생성
    email로 특정 Member가 존재하는지 검증하는 로직입니다.
    Member가 존재하면 예외를 던집니다.

--- server/src/main/java/com/main19/server/member/repository/MemberRepository.java
+++ server/src/main/java/com/main19/server/member/repository/MemberRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.main19.server.member.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
