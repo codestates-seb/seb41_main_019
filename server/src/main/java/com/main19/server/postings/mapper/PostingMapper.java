aed28f5feeaa175dc6509d6119788a511b8698a4
gimhae_person <hyeino3o@gmail.com>
2023-01-10 17:26:11 +0900

    Fix : Posting조회 시 종속된 Comment의 좋아요 목록 안보이는 버그 수정

--- server/src/main/java/com/main19/server/postings/mapper/PostingMapper.java
+++ server/src/main/java/com/main19/server/postings/mapper/PostingMapper.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.main19.server.comment.dto.CommentDto;
 import com.main19.server.comment.entity.Comment;
+import com.main19.server.comment.like.dto.CommentLikeGetResponseDto;
+import com.main19.server.comment.like.entity.CommentLike;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -58,4 +60,8 @@ public interface PostingMapper {
 	@Mapping(source = "member.profileImage" , target = "profileImage")
 	@Mapping(source = "posting.postingId", target = "postingId")
 	CommentDto.Response commentsToCommentsResponseDto(Comment comment);
+
+	@Mapping(source = "member.memberId" , target = "memberId")
+	@Mapping(source = "member.userName" , target = "userName")
+	CommentLikeGetResponseDto commentLikeToCommentLikeResponseGetResponseDto(CommentLike commentLike);
 }
