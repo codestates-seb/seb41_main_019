9bcd606241063f9d86aa880c2438b24b60cc8e95
lmimoh <dlalsgns9039@gmail.com>
2023-01-11 16:12:04 +0900

    Feat : chat component 구현

--- client/src/components/public/Chat.js
+++ client/src/components/public/Chat.js
@@ -1,18 +1,24 @@
 import styled from "styled-components";
 import Search from "./Search";
+import Chatting from "./Chatting";
 
 const StyledChat = styled.div`
   position: fixed;
   right: 0px;
+  display: flex;
+  flex-direction: column;
   width: 400px;
   height: 100%;
   border-left: 1px solid #dbdbdb;
+  padding: 20px 20px 0px 20px;
+  gap: 20px;
 `;
 
 const Chat = () => {
   return (
     <StyledChat>
-      <Search label={"검색"} />
+      <Search label={"채팅"} />
+      <Chatting />
     </StyledChat>
   );
 };
