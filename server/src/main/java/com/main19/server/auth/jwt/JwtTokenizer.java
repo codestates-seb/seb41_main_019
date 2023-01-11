76c7198a5ca1a9f86d5b7fdbfe779a075bd4ec42
TaeHeyeon_Kim <thk98k@gmail.com>
2023-01-11 12:08:47 +0900

    Feat : JwtTokenizer 구현
    1. 로그인 인증에 성공한 클라이언트에게 JWT를 생성 및 발급합니다.
    2. 클라이언트에서 요청이 들어올 때마다 전달된 JWT를 검증하는 역할을 합니다.

--- server/src/main/java/com/main19/server/auth/jwt/JwtTokenizer.java
+++ server/src/main/java/com/main19/server/auth/jwt/JwtTokenizer.java
@@ -1,9 +1,14 @@
 package com.main19.server.auth.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
@@ -11,7 +16,19 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 
+@Component
 public class JwtTokenizer {
+    @Getter
+    @Value("${jwt.key}")
+    private String secretKey;
+
+    @Getter
+    @Value("${jwt.access-token-expiration-minutes}")
+    private int accessTokenExpirationMinutes;
+
+    @Getter
+    @Value("${jwt.refresh-token-expiration-minutes}")
+    private int refreshTokenExpirationMinutes;
 
     public String encodeBase64SecretKey(String secretKey) {
         return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
@@ -40,6 +57,34 @@ public class JwtTokenizer {
                 .setExpiration(expiration)
                 .signWith(key)
                 .compact();
+    }
+
+        public Jws<Claims> getClaims(String jws, String base64EncodedSecretKey) {
+            Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
+
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jws);
+
+            return claims;
+        }
+
+        public void verifySignature(String jws, String base64EncodedSecretKey) {
+            Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
+
+        Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jws);
+    }
+
+    public Date getTokenExpiration(int expirationMinutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, expirationMinutes);
+        Date expiration = calendar.getTime();
+
+        return expiration;
     }
 
     private Key getKeyFromBase64EncodedKey(String base64EncodedSecretKey) {
