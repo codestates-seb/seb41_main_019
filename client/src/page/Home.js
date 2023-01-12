41652923de5f22ed332922818ed881ae014b7ebf
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-01-12 11:01:20 +0900

    Fix : sidebar component fix (#17)

--- client/src/page/Home.js
+++ client/src/page/Home.js
@@ -3,10 +3,10 @@ import Feeds from "../components/Home/Feeds";
 import styled from "styled-components";
 
 const StyledMain = styled.main`
-    margin: 0px 0px 0px 270px;
+    margin: 0px 300px 0px 270px;
 
     @media screen and (max-width: 1255px) {
-        margin: 0px 0px 0px 60px;
+        margin: 0px 300px 0px 60px;
     }
 
     @media screen and (max-width: 770px) {
