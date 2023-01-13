ce8691c95d0e3b00996983534575778fc7ffbc93
kanglaeheon <kanglaeheon@gmail.com>
2023-01-13 14:54:58 +0900

    Feat: Background 컴포넌트 어두운 배경 클릭시 isCovered 상태 변경

--- client/src/components/public/Background.js
+++ client/src/components/public/Background.js
@@ -2,24 +2,27 @@ import { useEffect } from "react";
 import styled from "styled-components";
 
 const StyledBackground = styled.div`
-    width: 100%;
-    height: 100%;;
-    position: fixed;
-    top:0;
-    left:0;
-    background: rgba(0,0,0,0.6);
-    z-index: 999;
-    display: ${({isCovered}) => isCovered ? null : "none"};
-`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.6);
+  z-index: 999;
+  display: ${({ isCovered }) => (isCovered ? null : "none")};
+`;
 
-const Background = ({ isCovered }) => {
-    useEffect(() => {
-        document.body.style.overflow = isCovered ? "hidden" : "auto";
-    }, [isCovered])
+const Background = ({ isCovered, handleIsCovered }) => {
+  useEffect(() => {
+    document.body.style.overflow = isCovered ? "hidden" : "auto";
+  }, [isCovered]);
 
-    return (
-        <StyledBackground isCovered={isCovered}></StyledBackground>
-    );
+  return (
+    <StyledBackground
+      isCovered={isCovered}
+      onClick={handleIsCovered}
+    ></StyledBackground>
+  );
 };
 
 export default Background;
