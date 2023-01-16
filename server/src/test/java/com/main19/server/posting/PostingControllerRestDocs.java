0438b20df3efe63ba633f1307cf51b6020297a64
gimhae_person <hyeino3o@gmail.com>
2023-01-16 13:45:08 +0900

    Test : PostingController Test 코드 에러 수정

--- server/src/test/java/com/main19/server/posting/PostingControllerRestDocs.java
+++ server/src/test/java/com/main19/server/posting/PostingControllerRestDocs.java
@@ -91,13 +91,16 @@ public class PostingControllerRestDocs {
         String postingContent = "게시글 test";
         List<String> tagName = new ArrayList<>();
         tagName.add("스투키");
+        tagName.add("몬스테라");
 
         PostingPostDto post = new PostingPostDto(memberId, postingContent, tagName);
 
-        PostingTagsResponseDto tag = new PostingTagsResponseDto("스투키");
+        PostingTagsResponseDto tag1 = new PostingTagsResponseDto("스투키");
+        PostingTagsResponseDto tag2 = new PostingTagsResponseDto("몬스테라");
 
         List<PostingTagsResponseDto> tags = new ArrayList<>();
-        tags.add(tag);
+        tags.add(tag1);
+        tags.add(tag2);
 
         String content = gson.toJson(post);
 
@@ -151,9 +154,10 @@ public class PostingControllerRestDocs {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.data.memberId").value(post.getMemberId()))
                 .andExpect(jsonPath("$.data.postingContent").value(post.getPostingContent()))
-//                .andExpect(jsonPath("$.data.tags").value(tags))
+                .andExpect(jsonPath("$.data.tags[0].tagName").value(post.getTagName().get(0)))
+                .andExpect(jsonPath("$.data.tags[1].tagName").value(post.getTagName().get(1)))
                 .andDo(document(
-                        "posting",
+                        "post-posting",
                         getRequestPreProcessor(),
                         getResponsePreProcessor(),
                         requestHeaders(
@@ -161,7 +165,7 @@ public class PostingControllerRestDocs {
                         ),
                         requestParts(
                                 RequestDocumentation.partWithName("multipartFiles").description("첨부파일"),
-                                RequestDocumentation.partWithName("requestBody").description("포스팅 내용")
+                                RequestDocumentation.partWithName("requestBody").description("게시글 내용")
                         ),
                         requestPartFields(
                                 "requestBody",
@@ -170,7 +174,7 @@ public class PostingControllerRestDocs {
                                         fieldWithPath("tagName").type(JsonFieldType.ARRAY).description("태그 이름")
                                 )
                         ),
-                        relaxedResponseFields(
+                        responseFields(
                                 fieldWithPath("data.postingId").type(JsonFieldType.NUMBER).description("게시글 식별자"),
                                 fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
                                 fieldWithPath("data.userName").type(JsonFieldType.STRING).description("회원 닉네임"),
@@ -179,7 +183,7 @@ public class PostingControllerRestDocs {
                                 fieldWithPath("data.postingMedias").type(JsonFieldType.ARRAY).description("첨부파일 리스트"),
                                 fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("작성일"),
                                 fieldWithPath("data.modifiedAt").type(JsonFieldType.STRING).description("최종 수정일"),
-                                fieldWithPath("data.tags").type(JsonFieldType.ARRAY).description("태그 이름"),
+                                fieldWithPath("data.tags[].tagName").type(JsonFieldType.STRING).description("태그 이름"),
                                 fieldWithPath("data.likeCount").type(JsonFieldType.NUMBER).description("좋아요 합계"),
                                 fieldWithPath("data.postingLikes").type(JsonFieldType.ARRAY).description("좋아요 누른 회원 리스트"),
                                 fieldWithPath("data.commentCount").type(JsonFieldType.NUMBER).description("댓글 합계"),
@@ -196,12 +200,16 @@ public class PostingControllerRestDocs {
         String postingContent = "게시글 수정 test";
         List<String> tagName = new ArrayList<>();
         tagName.add("스투키");
+        tagName.add("몬스테라");
 
         PostingPatchDto patch = new PostingPatchDto(postingId, postingContent, tagName);
-        PostingTagsResponseDto tag = new PostingTagsResponseDto("스투키");
+
+        PostingTagsResponseDto tag1 = new PostingTagsResponseDto("스투키");
+        PostingTagsResponseDto tag2 = new PostingTagsResponseDto("몬스테라");
 
         List<PostingTagsResponseDto> tags = new ArrayList<>();
-        tags.add(tag);
+        tags.add(tag1);
+        tags.add(tag2);
 
         String content = gson.toJson(patch);
 
@@ -248,13 +256,17 @@ public class PostingControllerRestDocs {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.postingId").value(patch.getPostingId()))
                 .andExpect(jsonPath("$.data.postingContent").value(patch.getPostingContent()))
-//                .andExpect(jsonPath("$.data.tags").value(patch.getTagName()))
+                .andExpect(jsonPath("$.data.tags[0].tagName").value(patch.getTagName().get(0)))
+                .andExpect(jsonPath("$.data.tags[1].tagName").value(patch.getTagName().get(1)))
                 .andDo(document(
-                        "patch",
+                        "patch-posting",
                         getRequestPreProcessor(),
                         getResponsePreProcessor(),
                         requestHeaders(
                                 headerWithName("Authorization").description("Bearer AccessToken")
+                        ),
+                        pathParameters(
+                                parameterWithName("posting-id").description("게시글 식별자")
                         ),
                         requestFields(
                                 List.of(fieldWithPath("postingId").type(JsonFieldType.NUMBER).description("게시글 식별자"),
@@ -262,7 +274,7 @@ public class PostingControllerRestDocs {
                                         fieldWithPath("tagName").type(JsonFieldType.ARRAY).description("태그 이름")
                                 )
                         ),
-                        relaxedResponseFields(
+                        responseFields(
                                 fieldWithPath("data.postingId").type(JsonFieldType.NUMBER).description("게시글 식별자"),
                                 fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
                                 fieldWithPath("data.userName").type(JsonFieldType.STRING).description("회원 닉네임"),
@@ -271,7 +283,7 @@ public class PostingControllerRestDocs {
                                 fieldWithPath("data.postingMedias").type(JsonFieldType.ARRAY).description("첨부파일 리스트"),
                                 fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("작성일"),
                                 fieldWithPath("data.modifiedAt").type(JsonFieldType.STRING).description("최종 수정일"),
-                                fieldWithPath("data.tags").type(JsonFieldType.ARRAY).description("태그 이름"),
+                                fieldWithPath("data.tags[].tagName").type(JsonFieldType.STRING).description("태그 이름"),
                                 fieldWithPath("data.likeCount").type(JsonFieldType.NUMBER).description("좋아요 합계"),
                                 fieldWithPath("data.postingLikes").type(JsonFieldType.ARRAY).description("좋아요 누른 회원 리스트"),
                                 fieldWithPath("data.commentCount").type(JsonFieldType.NUMBER).description("댓글 합계"),
@@ -280,4 +292,32 @@ public class PostingControllerRestDocs {
                         )
                 ));
     }
+
+//    @Test
+//    public void getPostingTest() throws Exception {
+//        // given
+//        long postingId = 1L;
+//        PostingResponseDto response =
+//                new PostingResponseDto(
+//                        postingId,
+//                        1L,
+//                        "gimhae_person",
+//                        "image",
+//                        "게시글 test",
+//                        new ArrayList<>(),
+//                        LocalDateTime.now(),
+//                        LocalDateTime.now(),
+//                        new ArrayList<>(),
+//                        0L,
+//                        new ArrayList<>(),
+//                        0L,
+//                        new ArrayList<>(),
+//                        new ArrayList<>()
+//                );
+//
+//        // when
+//
+//
+//        // then
+//    }
 }
