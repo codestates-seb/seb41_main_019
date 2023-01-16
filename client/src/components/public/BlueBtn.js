9f8ca0c31eaa5cf7c11ea707859f3099fb04b060
Eun-ji의 macbook <jangeun-ji@Eunji-MacBook-Pro.local>
2023-01-17 01:47:52 +0900

    Design : button color 변경

--- client/src/components/public/BlueBtn.js
+++ client/src/components/public/BlueBtn.js
@@ -9,5 +9,5 @@ export const BlueBtn = styled.button`
     margin-right: 10px;
     box-shadow: 1px 3px 8px -2px rgb(90, 90, 90);
     background-color: #2F4858;
-    color: white;
+    color: #ededed;
 `;
