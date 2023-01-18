8d55e419affc3fdbb5978e658aeef6ddc0c7d4b0
oheadnah <kanmini@naver.com>
2023-01-18 11:14:39 +0900

    Test : Posting Response 에 image 추가

--- server/src/main/java/com/main19/server/posting/dto/MediaResponseDto.java
+++ server/src/main/java/com/main19/server/posting/dto/MediaResponseDto.java
@@ -1,9 +1,11 @@
 package com.main19.server.posting.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 @Builder
 public class MediaResponseDto {
 	private long mediaId;
