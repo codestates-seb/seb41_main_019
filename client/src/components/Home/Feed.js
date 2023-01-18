4b9234bad956bf3967853bb0ae049b8dd1aa9156
eunji <omegle3333@gmail.com>
2023-01-18 18:36:30 +0900

    Feat : exchangeTime func feat

--- client/src/components/Home/Feed.js
+++ client/src/components/Home/Feed.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import Post from "./Post";
 import { ReactComponent as NoContent } from "../../assets/svg/monstera.svg";
+import Cookie from "../../util/Cookie";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Wrapper = styled.section`
     position: relative;
@@ -23,17 +26,32 @@ const Wrapper = styled.section`
 `
 
 const Feed = ({ handleModal, handleDelete }) => { 
-    const datas = new Array(0).fill(0);
+    const [ posts, setPosts ] = useState([]);
+    const cookie = new Cookie();
+
+    useEffect(() => {
+        axios({
+            method: "get",
+            url: "http://13.124.33.113:8080/posts?page=1&size=10",
+            headers: { Authorization: cookie.get("authorization") }
+            }).then(res => {
+                setPosts(res.data.data);
+            })
+            .catch(e => {
+               console.log(e);
+            });
+    }, [])
+
 
     return (
         <Wrapper>
-            { datas.length < 1 
+            { posts.length < 1 
                 ?
                     <div className="no-content"> 
                         <NoContent />
                         <span>게시물이 없습니다.</span> 
                     </div>
-                : datas.map((data,idx) => <Post key={idx} handleModal={handleModal} handleDelete={handleDelete} />)
+                : posts.map((post, idx) => <Post post={post} key={idx} handleModal={handleModal} handleDelete={handleDelete} />)
             }
         </Wrapper>
     )
--- client/src/components/Home/Post.js
+++ client/src/components/Home/Post.js
@@ -78,7 +78,8 @@ const StyledHeader = styled.div`
 `;
 
 
-const Post = ({ handleModal, handleDelete }) => {
+const Post = ({ post, handleModal, handleDelete }) => {
+    // console.log(post);
     const [menu, setMenu] = useState(false);
 
     const handleMenu = () => {
@@ -87,14 +88,57 @@ const Post = ({ handleModal, handleDelete }) => {
     
     const img = [ A, B ];
 
+    const exchangeTime = () => {
+        //일 차이 계산
+        const nDay = new Date().toISOString().slice(0, 10).split("-")[2];
+        const pDay = post.modifiedAt.slice(0, 10).split("-")[2];
+
+        const day = (nDay - pDay) * 24 * 60 * 60;
+
+        //시간 차이 계산
+        const nTime = new Date().toISOString().slice(11, 19).split(":").map((el, idx) => {
+            switch(idx) {
+                case 0 : return Number(el) * 60 * 60;
+                case 1 : return Number(el) * 60;
+                case 2 : return Number(el);
+                default : break;
+            }
+        }).reduce((acc, cur) => acc + cur);
+
+        const pTime = post.modifiedAt.slice(11, 19).split(":").map((el, idx) => {
+            switch(idx) {
+                case 0 : return Number(el) * 60 * 60;
+                case 1 : return Number(el) * 60;
+                case 2 : return Number(el);
+                default : break;
+            }
+        }).reduce((acc, cur) => acc + cur);
+
+        // 일 차이 반영
+        const differ = nTime - pTime + day;
+
+        // 최대 시간 차로 변환
+        const hour = parseInt(differ / 60 / 60);
+        const minute = parseInt(differ / 60 % 60);
+        const second = differ % 60;
+
+        if(hour > 0) {
+            return `${hour}시간 전`;
+        } else if(minute > 0) {
+            return `${minute}분 전`;
+        } else {
+            return `${second}초 전`;
+        }
+    }
+
     return (
         <Wrapper>
             { menu ? <FeedMenu handleDelete={handleDelete} handleMenu={handleMenu} /> : null }
             <StyledHeader>
-                <img src={A} alt="img" />
+                <img src={post.profileImage} alt="img" />
                 <div>
-                    <span>홍길동</span>
-                    <span>7시간 전</span>
+                    <span>{post.userName}</span>
+                    <span>{exchangeTime()}</span>
                 </div>
                 <div className="icons">
                     <FiUserPlus />
