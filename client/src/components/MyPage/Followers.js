28544b46be05db5d3bc0b6688cfa9ecbe181dadb
kanglaeheon <kanglaeheon@gmail.com>
2023-01-28 14:06:58 +0900

    Feat : Follwer 목록 조회 Modal 추가

--- client/src/components/MyPage/Followers.js
+++ client/src/components/MyPage/Followers.js
@@ -1,15 +1,41 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
+import FollowerItem from "./FollwersItem";
 
 const StyledContainer = styled.div`
-    position: absolute;
-    top:50%;
-    left:50%;
-    transform:translate(-50%, -50%);
-    background-color: white;
-    width: 1240px;
-    height: 900px;
-    z-index: 1000;
+  position: absolute;
+  top:50%;
+  left:50%;
+  transform:translate(-50%, -50%);
+  background-color: white;
+  width: 400px;
+  min-height : 500px;
+  padding: 20px;
+  z-index: 1000;
+
+  ul  {
+   margin: 0px;
+    padding: 0px;
+    list-style: none;
+    overflow: scroll;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+
+    li: last-child {
+      margin: 0px;
+    }
+  }
+
+  > p {
+    margin: 0px;
+    font-size: 18px;
+    font-weight: 600;
+    margin: 0px 0px 20px 0px;
+    padding-bottom: 20px;
+    border-bottom: 1px solid #dbdbdb;
+  }
+
 `
 
 const Followers = ({handleFollowers, followers}) => {
@@ -24,6 +50,12 @@ const Followers = ({handleFollowers, followers}) => {
   return (
       <StyledContainer>
         <p>팔로워 목록</p>
+        <ul>
+          {followers.length > 0 
+            ? followers.map((follower, idx) => (
+              <FollowerItem follower={follower} key={follower.followerId}/>
+          )) : "팔로워가 없습니다."}
+        </ul>
       </StyledContainer>
   )
 }
