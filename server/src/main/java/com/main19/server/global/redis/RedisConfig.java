2246faa3d46703dade3271e440ee8d130db9d583
oheadnah <107042410+oheadnah@users.noreply.github.com>
2023-02-03 03:52:10 +0900

    Refactor : redis config 수정

--- server/src/main/java/com/main19/server/global/redis/RedisConfig.java
+++ server/src/main/java/com/main19/server/global/redis/RedisConfig.java
@@ -35,7 +35,6 @@ public class RedisConfig {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(redisHost);
         redisStandaloneConfiguration.setPort(redisPort);
-        
         Arrays.stream(environment.getActiveProfiles()).forEach(profile -> {
                 if (!profile.equals("server")) {
                     redisStandaloneConfiguration.setPassword(password);
