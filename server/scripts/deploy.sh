809618afb1c5a53f7d63a3d7e7ad2c0b0713493c
oheadnah <107042410+oheadnah@users.noreply.github.com>
2023-02-03 09:09:32 +0900

    Update deploy.sh

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -14,12 +14,24 @@ cp $BUILD_JAR $DEPLOY_PATH
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
+CURRENT_PID=$(pgrep -f redis-server-2.8.19)
+
 if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
 else
   echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
   sudo kill -9 $CURRENT_PID
+  sleep 5
+fi
+
+if [ -z $CURRENT_PIDD ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
+else
+  echo "> kill -9 $CURRENT_PIDD" >> /home/ubuntu/action/deploy.log
+  sudo kill -9 $CURRENT_PIDD
   sleep 5
 fi
 
